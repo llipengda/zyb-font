cda5d3f9819fe23dacddc739dd9e3d8baf27541d
llipengda <lipengda2@outlook.com>
2023-12-13 03:26:29 +0800

    Add directory change to root path

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ import deeplearning
 import gui
 
 
+src_path = os.path.dirname(os.path.abspath(__file__))
+root_path = os.path.dirname(src_path)
+os.chdir(root_path)
+
 if __name__ == "__main__":
     if not os.path.exists("out/model.pth"):
         deeplearning.train_and_test(10)
