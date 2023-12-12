4e13fb78178d0fd6a17ac3875ac39eaff5342c0e
llipengda <lipengda2@outlook.com>
2023-12-12 21:55:47 +0800

    update requirements.txt and settings

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import os
+
 import gui
+
+src_path = os.path.dirname(os.path.abspath(__file__))
+root_path = os.path.dirname(src_path)
+os.chdir(root_path)
 
 if __name__ == '__main__':
     gui.paint.run()
