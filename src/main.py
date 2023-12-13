46e3875fafc2d1ff5ac43c2609f25ca5ce123a0e
XiaoWu <1610365132@qq.com>
2023-12-13 16:44:17 +0800

    11

--- src/main.py
+++ src/main.py
@@ -1,13 +1,4 @@
-import os
+import gui
 
-import deeplearning
-
-
-if __name__ == "__main__":
-    if not os.path.exists("out/model.pth"):
-        deeplearning.train_and_test()
-    
-    predict = deeplearning.Pridict()
-    for file in os.listdir("draw"):
-        res = predict(f"draw/{file}")
-        print(f"{file} is {res}")
+if __name__ == '__main__':
+    gui.paint.run()
