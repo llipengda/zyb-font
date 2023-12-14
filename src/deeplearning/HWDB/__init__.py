4b3d077d34418df9d873ae55292affb9b0122f5d
llipengda <lipengda2@outlook.com>
2023-12-14 17:08:47 +0800

    Add train_and_test function and refactor run function

--- src/deeplearning/HWDB/__init__.py
+++ src/deeplearning/HWDB/__init__.py
@@ -1,1 +1,8 @@
-__all__ = []
+from deeplearning.HWDB.__main__ import run
+
+
+def train_and_test(epochs=10):
+    run(epochs)
+
+
+__all__ = ['train_and_test']
--- src/deeplearning/HWDB/__main__.py
+++ src/deeplearning/HWDB/__main__.py
@@ -1,10 +1,23 @@
+import os
+
 from deeplearning.HWDB.Train import Train
+from deeplearning.HWDB.fetch_data import fetch_data
+from deeplearning.HWDB.process_data import load_img_from_gnt
 
 
-def train_and_test():
-    train = Train()
+def run(epochs=10):
+    if not os.path.exists('data/HWDB/test') or not os.path.exists('data/HWDB/train') \
+            or len(os.listdir('data/HWDB/test')) == 0 or len(os.listdir('data/HWDB/train')) == 0:
+
+        if not os.path.exists('data/HWDB/raw/HWDB1.1trn_gnt') or not os.path.exists('data/HWDB/raw/HWDB1.1tst_gnt') \
+                or len(os.listdir('data/HWDB/raw/HWDB1.1trn_gnt')) == 0 or len(os.listdir('data/HWDB/raw/HWDB1.1tst_gnt')) == 0:
+            fetch_data()
+
+        load_img_from_gnt()
+
+    train = Train(epochs)
     train()
 
 
 if __name__ == "__main__":
-    train_and_test()
+    run()
