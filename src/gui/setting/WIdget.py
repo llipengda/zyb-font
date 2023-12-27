675f3baaf4581cf3fc9f537c35a73d3b8859eae1
XiaoWu <1610365132@qq.com>
2023-12-27 17:16:47 +0800

    better ui

--- src/gui/setting/WIdget.py
+++ src/gui/setting/WIdget.py
@@ -35,11 +35,22 @@ class Widget(QWidget):
         self.combo_box.currentIndexChanged.connect(self.on_index_changed)
         self.sub_layout.addWidget(self.combo_box)
 
+        self.sub_layout = QHBoxLayout()
+        self.sub_layout.setContentsMargins(10, 10, 10, 10)
+        self.sub_layout.setSpacing(10)
+        self.layout.addLayout(self.sub_layout)
+
         self.clear_files = Button("清空图库")
         self.clear_files.setParent(self)
         self.clear_files.setMaximumWidth(120)
         self.clear_files.pressed.connect(self.on_clear_files)
-        self.layout.addWidget(self.clear_files)
+        self.sub_layout.addWidget(self.clear_files)
+
+        self.clear_log = Button("清空日志")
+        self.clear_log.setParent(self)
+        self.clear_log.setMaximumWidth(120)
+        self.clear_log.pressed.connect(self.on_clear_log)
+        self.sub_layout.addWidget(self.clear_log)
 
     def on_index_changed(self, value):
         self.idx.emit(value)
@@ -51,5 +62,15 @@ class Widget(QWidget):
         if os.path.exists("draw"):
             for file in os.listdir("draw"):
                 os.remove(f"draw/{file}")
+
+        self.setCursor(Qt.ArrowCursor)
+
+    def on_clear_log(self):
+        on_pressed(self.clear_log)
+        self.setCursor(Qt.WaitCursor)
+
+        if os.path.exists("logs"):
+            for file in os.listdir("logs"):
+                os.remove(f"logs/{file}")
 
         self.setCursor(Qt.ArrowCursor)
